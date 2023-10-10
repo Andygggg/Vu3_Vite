7bba5e28d1da4ff6c95ba4f670baea0180c0e905
andygggg <andyhello31468@gmail.com>
2023-10-11 00:37:55 +0800

    第一次push到github

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting',
+  ],
+  rules: {
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'only-multiline',
+      },
+    ],
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+}
---
+++ src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button @click="add(1)">test</button>
+    <h1>{{ test.name }} {{ test.age }} {{ envTest }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const envTest = ref('')
+const test = ref({
+  name: 'andy',
+  age: '12',
+})
+
+function add(i) {
+  console.log(i)
+  console.log(i + 1)
+}
+onMounted(() => {
+  envTest.value = import.meta.env.VITE_APP_TITLE
+})
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
